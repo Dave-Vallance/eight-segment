276076f02ceda2333fae77e1f4570b487e353128
Dave Vallance <dave.yna@googlemail.com>
2023-06-23 20:28:20 +0100

    Update to support digital::v2
    Was getting some workings and the Output pins would not work with my hal
    
    - Updated to digital v2
    - Updated Output pin to return the error type Infallible. (seemed to be required)
    - Added pi pico example

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 extern crate embedded_hal;
 
-use embedded_hal::digital::OutputPin;
+use embedded_hal::digital::v2::OutputPin;
 
 ///An eight segment display that can display a single digit from 0x0 to 0xF at a time.
 /// Intended for use with the HDSP-H101 and HDSP-H103.
@@ -50,57 +50,110 @@ use embedded_hal::digital::OutputPin;
 ///    eight_segment.blank(); // All segments off
 ///    eight_segment.display(0xb, false); // Display 'b' with decimal point off
 ///```
-
+/// # RP PICO
+///``` rust,ignore
+///     // using the `rp_pico` crate that re-exports the rp2040_hal crate
+///     use rp_pico 
+///     use rp_pico::hal::pac
+///     use rp_pico::hal::sio::Sio
+///     use eight_segment::EightSegment;
+///     
+///     let mut pac = pac::Peripherals::take().unwrap();
+///     let sio = Sio::new(pac.SIO);
+/// 
+///     let pins = rp_pico::Pins::new(
+///         pac.IO_BANK0,
+///         pac.PADS_BANK0,
+///         sio.gpio_bank0,
+///         &mut pac.RESETS,
+///     );
+///     // Pins 6-9 are the the bottom four pins on the eight segment display
+///     // Pins 10-13 are the top four pins on the eight segment display
+///     let mut seg_a = pins.gpio8.into_push_pull_output();
+///     let mut seg_b = pins.gpio9.into_push_pull_output();
+///     let mut seg_c = pins.gpio12.into_push_pull_output();
+///     let mut seg_d = pins.gpio11.into_push_pull_output();
+///     let mut seg_e = pins.gpio10.into_push_pull_output();
+///     let mut seg_f = pins.gpio7.into_push_pull_output();
+///     let mut seg_g = pins.gpio6.into_push_pull_output();
+///     let mut seg_p = pins.gpio13.into_push_pull_output();
+///
+///     let mut eight_segment = EightSegment {
+///         high_on: true, 
+///         seg_a: &mut seg_a,
+///         seg_b: &mut seg_b,
+///         seg_c: &mut seg_c,
+///         seg_d: &mut seg_d,
+///         seg_e: &mut seg_e,
+///         seg_f: &mut seg_f,
+///         seg_g: &mut seg_g,
+///         seg_p: &mut seg_p,
+///         };
+/// 
+///    eight_segment.blank(); // All segments off
+///    eight_segment.display(0xb, false); // Display 'b' with decimal point off
+///```
+/// 
+use embedded_hal::digital::v2::PinState;
 pub struct EightSegment<'a> {
     pub high_on: bool,
-    pub seg_a: &'a mut OutputPin,
-    pub seg_b: &'a mut OutputPin,
-    pub seg_c: &'a mut OutputPin,
-    pub seg_d: &'a mut OutputPin,
-    pub seg_e: &'a mut OutputPin,
-    pub seg_f: &'a mut OutputPin,
-    pub seg_g: &'a mut OutputPin,
-    pub seg_p: &'a mut OutputPin,
+    pub seg_a: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
+    pub seg_b: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
+    pub seg_c: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
+    pub seg_d: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
+    pub seg_e: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
+    pub seg_f: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
+    pub seg_g: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
+    pub seg_p: &'a mut dyn OutputPin<Error = core::convert::Infallible>,
 }
 
 impl<'a> EightSegment<'a> {
     pub fn blank(&mut self) {
-        if self.high_on {self.seg_a.set_low()} else {self.seg_a.set_high()};
-        if self.high_on {self.seg_b.set_low()} else {self.seg_b.set_high()};
-        if self.high_on {self.seg_c.set_low()} else {self.seg_c.set_high()};
-        if self.high_on {self.seg_d.set_low()} else {self.seg_d.set_high()};
-        if self.high_on {self.seg_e.set_low()} else {self.seg_e.set_high()};
-        if self.high_on {self.seg_f.set_low()} else {self.seg_f.set_high()};
-        if self.high_on {self.seg_g.set_low()} else {self.seg_g.set_high()};
-        if self.high_on {self.seg_p.set_low()} else {self.seg_p.set_high()};
+        let _ = self.seg_a.set_state(PinState::from(self.high_on));
+        let _ = self.seg_b.set_state(PinState::from(self.high_on));
+        let _ = self.seg_c.set_state(PinState::from(self.high_on));
+        let _ = self.seg_d.set_state(PinState::from(self.high_on));
+        let _ = self.seg_e.set_state(PinState::from(self.high_on));
+        let _ = self.seg_f.set_state(PinState::from(self.high_on));
+        let _ = self.seg_g.set_state(PinState::from(self.high_on));
+        let _ = self.seg_p.set_state(PinState::from(self.high_on));
     }
 
-    pub fn set_segments(&mut self,
-                        seg_a_on: bool,
-                        seg_b_on: bool,
-                        seg_c_on: bool,
-                        seg_d_on: bool,
-                        seg_e_on: bool,
-                        seg_f_on: bool,
-                        seg_g_on: bool,
-                        seg_p_on: bool) {
-
-        if seg_a_on ^ !self.high_on {self.seg_a.set_high()} else {self.seg_a.set_low()};
-        if seg_b_on ^ !self.high_on {self.seg_b.set_high()} else {self.seg_b.set_low()};
-        if seg_c_on ^ !self.high_on {self.seg_c.set_high()} else {self.seg_c.set_low()};
-        if seg_d_on ^ !self.high_on {self.seg_d.set_high()} else {self.seg_d.set_low()};
-        if seg_e_on ^ !self.high_on {self.seg_e.set_high()} else {self.seg_e.set_low()};
-        if seg_f_on ^ !self.high_on {self.seg_f.set_high()} else {self.seg_f.set_low()};
-        if seg_g_on ^ !self.high_on {self.seg_g.set_high()} else {self.seg_g.set_low()};
-        if seg_p_on ^ !self.high_on {self.seg_p.set_high()} else {self.seg_p.set_low()};
+    pub fn set_segments(
+        &mut self,
+        seg_a_on: bool,
+        seg_b_on: bool,
+        seg_c_on: bool,
+        seg_d_on: bool,
+        seg_e_on: bool,
+        seg_f_on: bool,
+        seg_g_on: bool,
+        seg_p_on: bool,
+    ) {
+        let _ = self.seg_a.set_state(PinState::from(seg_a_on ^ !self.high_on));
+        let _ = self.seg_b.set_state(PinState::from(seg_b_on ^ !self.high_on));
+        let _ = self.seg_c.set_state(PinState::from(seg_c_on ^ !self.high_on));
+        let _ = self.seg_d.set_state(PinState::from(seg_d_on ^ !self.high_on));
+        let _ = self.seg_e.set_state(PinState::from(seg_e_on ^ !self.high_on));
+        let _ = self.seg_f.set_state(PinState::from(seg_f_on ^ !self.high_on));
+        let _ = self.seg_g.set_state(PinState::from(seg_g_on ^ !self.high_on));
+        let _ = self.seg_p.set_state(PinState::from(seg_p_on ^ !self.high_on));
     }
 
     pub fn display(&mut self, count: u8, seg_p_on: bool) {
-        let (seg_a_on, seg_f_on, seg_b_on, seg_g_on, seg_e_on, seg_c_on, seg_d_on) = match count {
+        let (
+            seg_a_on,
+            seg_f_on,
+            seg_b_on,
+            seg_g_on,
+            seg_e_on,
+            seg_c_on,
+            seg_d_on,
+        ) = match count {
             0x0 => (true, true, true, false, true, true, true),
             0x1 => (false, false, true, false, false, true, false),
             0x2 => (true, false, true, true, true, false, true),
-            0x3 => (true, false,  true, true, false, true, true),
+            0x3 => (true, false, true, true, false, true, true),
             0x4 => (false, true, true, true, false, true, false),
             0x5 => (true, true, false, true, false, true, true),
             0x6 => (true, true, false, true, true, true, true),
@@ -124,7 +177,7 @@ impl<'a> EightSegment<'a> {
             seg_e_on,
             seg_f_on,
             seg_g_on,
-            seg_p_on
+            seg_p_on,
         );
     }
 }
